cbbb41beaaad9310df31d2b8df9b4869ade7f056
Hyunwoo Lee <baekdam8@gmail.com>
2022-09-14 18:59:26 +0900

    docker-compose mongodb container name modified

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
   await app.listen(3000);
+  // console.log(ConfigService.get<string>('MONGODB_URI'));
 }
 bootstrap();
